215d820832a76fc83e757fe22424b9e5403006f2
BrianSammit <briansammit.cruz@gmail.com>
2020-09-14 17:42:47 -0500

    Add a method my_each_with_index

--- my_each_index_test.rb
+++ my_each_index_test.rb
@@ -1,10 +1,10 @@
 module Enumerable
-    def my_each()
+    def my_each_index()
         # build a loop around the given array
         n = self.length
         i = 0
         while i < n
-          yield self[i] # [1, 2, 3]
+          yield self[i], i # [1, 2, 3]
           i += 1
         end
       
@@ -13,7 +13,8 @@ module Enumerable
 end
 
 
-a = {"one" , "two"}
-a.my_each() do |key, value|
-    pp "#{key} is #{value}"
+a = [1,5,8,4,9]
+a.my_each_index() do |num, index|
+    pp "my number is #{num} and my index is #{index}"
   end
+
