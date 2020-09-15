91b71c4393bda1c0ee1fdcc3236f21f24339a5a4
BrianSammit <briansammit.cruz@gmail.com>
2020-09-15 11:26:09 -0500

    Add my_select method

---
+++ my_select.rb
@@ -0,0 +1,18 @@
+module Enumerable
+    def my_select ()
+        converted_array = self.to_a
+        i = 0
+        returned_array = []
+        while i < converted_array.length
+           conditional_met = yield converted_array[i]
+           if conditional_met == true
+            returned_array.push(converted_array[i])
+           end
+            i += 1
+        end
+       pp returned_array
+    end  
+end
+
+(1...100).my_select { |num|  num.even?  }  #[2, 4]
+
